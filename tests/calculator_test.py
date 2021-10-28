e035e97c895c8efe89d53b5edc4029ee1526a476
najmhassan <nhh4@njit.edu>
2021-10-27 22:05:28 -0400

    fixed divided by  0  test bug

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,49 +1,54 @@
 """Testing the Calculator"""
-from calculator.main import Calculator
+import pytest
+from calc.main import Calculator
+
 
 def test_calculator_result():
-    """testing calculator result is 0"""
+    """testing calc result is 0"""
     calc = Calculator()
     assert calc.result == 0
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    calc.add_number(1)
-    assert calc.get_result() == 1
 
 def test_calculator_add():
-    """Testing the Add function of the calculator"""
+    """Testing the Add function of the calc"""
     #Arrange by instantiating the calc class
     calc = Calculator()
     #Act by calling the method to be tested
-    calc.add_number(1)
+    calc.add_number(1,2)
     #Assert that the results are correct
-    assert calc.result == 1
+    assert calc.result == 3
 
 def test_calculator_subtract():
-    """Testing the subtract method of the calculator"""
+    """Testing the subtract method of the calc"""
     # Arrange
     calc = Calculator()
     # Act
-    calc.subtract_number(1)
+    calc.subtract_number(2,1)
     # Assert
-    assert calc.get_result() == -1
+    assert calc.get_result() == 1
 
 def test_calculator_multiply():
-    """Testing the multiplication method of the calculator"""
+    """Testing the multiplication method of the calc"""
     # Arrange
     calc = Calculator()
     # Act
-    calc.multiply_number(2)
+    calc.multiply_number(2,3)
+    # Assert
+    assert calc.get_result() == 6
+
+def test_calculator_divide():
+    """Testing the division method of the calc"""
+    # Arrange
+    calc = Calculator()
+    # Act
+    calc.divide_number(0,1)
     # Assert
     assert calc.get_result() == 0
 
-def test_calculator_divide():
-    """Testing the division method of the calculator"""
+def test_calculator_divide_by_0():
+    """Testing the division method by0  of the calc"""
     # Arrange
     calc = Calculator()
-    # Act
-    calc.divide_number(1)
-    # Assert
-    assert calc.get_result() == 0
+    # Act & Assert
+    with pytest.raises(ZeroDivisionError):
+        calc.divide_number(2, 0)
