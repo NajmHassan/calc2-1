651fcef1d75d6a67c0d8b8ea800597cd5af38563
najmhassan <nhh4@njit.edu>
2021-11-04 19:18:11 -0400

    testing history and calculations

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,54 +1,56 @@
 """Testing the Calculator"""
+import pprint
 import pytest
-from calc.main import Calculator
-
-
-def test_calculator_result():
-    """testing calc result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
-
+from calculator.calculator import Calculator
+@pytest.fixture
+def empty_history():
+    """clear history each time u run a test"""
+    Calculator.clear_history()
 
 def test_calculator_add():
-    """Testing the Add function of the calc"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(1,2)
-    #Assert that the results are correct
-    assert calc.result == 3
+    """Testing the Add function of the calculator"""
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
+
+def test_clear_history():
+    """test to clear history"""
+    Calculator.clear_history()
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.clear_history() is True
+    assert Calculator.history_count() == 0
+
+def test_count_history():
+    """test to count history"""
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.history_count() == 2
+
+def test_get_last_calculation_result():
+    """test to fet last calc result"""
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 5
+
+def test_get_first_calculation_result():
+    """tests to get first calc result"""
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
 
 def test_calculator_subtract():
-    """Testing the subtract method of the calc"""
-    # Arrange
-    calc = Calculator()
-    # Act
-    calc.subtract_number(2,1)
-    # Assert
-    assert calc.get_result() == 1
+    """Testing the subtract method of the calculator"""
+    assert Calculator.subtract_number(1, 2) == -1
 
 def test_calculator_multiply():
-    """Testing the multiplication method of the calc"""
-    # Arrange
-    calc = Calculator()
-    # Act
-    calc.multiply_number(2,3)
-    # Assert
-    assert calc.get_result() == 6
-
-def test_calculator_divide():
-    """Testing the division method of the calc"""
-    # Arrange
-    calc = Calculator()
-    # Act
-    calc.divide_number(0,1)
-    # Assert
-    assert calc.get_result() == 0
-
-def test_calculator_divide_by_0():
-    """Testing the division method by0  of the calc"""
-    # Arrange
-    calc = Calculator()
-    # Act & Assert
-    with pytest.raises(ZeroDivisionError):
-        calc.divide_number(2, 0)
+    """ tests multiplication of two numbers"""
+    assert Calculator.multiply_numbers(1,2) == 2
